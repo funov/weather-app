e7183378a4ffa2fd9e358af27462fa47b22c431a
Yana Kosareva <k.osareva@mail.ru>
2023-05-20 17:09:35 +0500

    delete markup and render property

--- app/static/scripts/script.js
+++ app/static/scripts/script.js
@@ -75,54 +75,7 @@ const getWeatherData = async (city, unit, hourlyOrWeek) => {
     });
 }
 
-const renderProperty = (properties) => {
-    return Object.values(properties)
-        .map(({title, value, icon}) => {
-            return `<div class="property">
-            <div class="property-icon">
-              <img src="./app/static/images/properties_icons/${icon}" alt="">
-            </div>
-            <div class="property-info">
-              <div class="property-info__value">${value}</div>
-              <div class="property-info__description">${title}</div>
-            </div>
-          </div>`;
-        })
-        .join("");
-};
 
-const markup = () => {
-    const {city, description, weatherTime, temperature, timezone, properties, iconId} = store;
-
-    let date = new Date(new Date((weatherTime + timezone) * 1000).toISOString());
-    let hours = date.getUTCHours();
-    let minutes = date.getUTCMinutes();
-    const localTime = `${hours}:${minutes}`;
-    const containerClass = hours > 6 && hours < 20 ? "is-day" : "";
-
-    return `<div class="container ${containerClass}">
-            <div class="top">
-              <div class="city">
-                <div class="city-subtitle">Погода сегодня</div>
-                  <div class="city-title" id="city">
-                  <span>${city}</span>
-                </div>
-              </div>
-              <div class="city-info">
-                <div class="top-left">
-                <img class="icon" src="./app/static/images/weather_icons/${iconId}.png" alt="" />
-                <div class="description">${description}</div>
-              </div>
-            
-              <div class="top-right">
-                <div class="city-info__subtitle">Время ${localTime}</div>
-                <div class="city-info__title">${temperature}°</div>
-              </div>
-            </div>
-          </div>
-        <div id="properties">${renderProperty(properties)}</div>
-      </div>`;
-};
 
 const togglePopupClass = () => {
     popup.classList.toggle("active");
