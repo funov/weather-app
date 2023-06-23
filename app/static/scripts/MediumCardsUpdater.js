ea02306bf8db6ccab13a0038e1e8c17a79b339fd
Danil_Titarenko <danil.titarenko@gmail.com>
2023-06-23 11:05:57 +0500

    updated metrics

--- app/static/scripts/MediumCardsUpdater.js
+++ app/static/scripts/MediumCardsUpdater.js
@@ -4,19 +4,20 @@ import {DocumentElements} from "./DocumentElements.js";
 let documentElements = new DocumentElements();
 
 
-
-
 export class MediumCardsUpdater{
     UpdateData(weather) {
         let statusUpdater = new StatusMediumCardsUpdater();
-        documentElements.uvIndex.innerText = weather.uvindex;
-        let windSpeed = weather.windspeed / 3.6;
+        documentElements.uvIndex.innerText = `${weather.uvIndex}`;
+        let windSpeed = weather.windSpeed / 3.6;
+        documentElements.feelsLike.innerText = `${weather.feelsLike}`;
         documentElements.windSpeed.innerText = `${Math.round(windSpeed)} м/с`;
         documentElements.humidity.innerText = `${weather.humidity} %`;
         documentElements.visibility.innerText = `${weather.visibility} км`;
+        documentElements.airQualityStatus.innerText = `${weather.airQualityStatus}`;
+        statusUpdater.updateAirQuality(weather.airQualityStatus);
         statusUpdater.updateVisibility(weather.visibility);
         statusUpdater.updateHumidity(weather.humidity);
-        statusUpdater.updateUvIndex(weather.uvindex);
+        statusUpdater.updateUvIndex(weather.uvIndex);
         statusUpdater.updateWindSpeed(windSpeed);
     }
 }
